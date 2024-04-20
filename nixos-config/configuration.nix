74e92cc6421c9f17e3591830fe2ea93e1db397dc
levi-manoel <levimanoel.deob@gmail.com>
2024-04-20 20:48:36 -0300

    feat: move bash config to nix configuration

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -1,11 +1,12 @@
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      <home-manager/nixos>
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -61,14 +62,14 @@
   users.users.levi = {
     isNormalUser = true;
     description = "levi";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       firefox
     ];
   };
 
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   environment.systemPackages = with pkgs; [
     google-chrome
@@ -86,6 +87,7 @@
     alejandra
     nil
     nixpkgs-fmt
+    gnome.dconf-editor
 
     nano
     curl
@@ -129,8 +131,7 @@
 
       zed-mono = mkZedFont "zed-mono" "sha256-k9N9kWK2JvdDlGWgIKbRTcRLMyDfYdf3d3QTlA1iIEQ=";
       zed-sans = mkZedFont "zed-sans" "sha256-BF18dD0UE8Q4oDEcCf/mBkbmP6vCcB2vAodW6t+tocs=";
-    in
-    [
+    in [
       victor-mono
       corefonts
       font-awesome
@@ -149,13 +150,12 @@
     fontconfig = {
       enable = true;
       defaultFonts = {
-        monospace = [ "Victor Mono" "Zed Mono" "Noto Sans Mono" ];
-        serif = [ "Noto Serif" "Source Han Serif" ];
-        sansSerif = [ "Zed Sans" "Noto Sans" "Source Han Sans" ];
+        monospace = ["Victor Mono" "Zed Mono" "Noto Sans Mono"];
+        serif = ["Noto Serif" "Source Han Serif"];
+        sansSerif = ["Zed Sans" "Noto Sans" "Source Han Sans"];
       };
     };
   };
-
 
   # SERVICES
   services.redis.servers = {
@@ -174,7 +174,7 @@
     driSupport32Bit = true;
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
     modesetting.enable = true;
@@ -194,4 +194,108 @@
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+
+  home-manager.users.levi = {pkgs, ...}: {
+    programs.home-manager.enable = true;
+    home.stateVersion = "23.11";
+    home.username = "levi";
+    home.homeDirectory = "/home/levi";
+
+    programs.git = {
+      enable = true;
+      userName = "levi-manoel";
+      userEmail = "levimanoel.doeb@gmail.com";
+
+      aliases = {
+        st = "status";
+        cmsg = "commit -m";
+
+        unstage = "restore --staged";
+        uncommit = "reset --soft HEAD~";
+
+        cp = "cherry-pick";
+
+        l = "log --oneline --no-merges";
+        ll = "log --graph --topo-order --date=short --abbrev-commit --decorate --all --boundary --pretty=format:'%Cgreen%ad %Cred%h%Creset -%C(yellow)%d%Creset %s %Cblue[%cn]%Creset %Cblue%G?%Creset'";
+        lll = "log --graph --topo-order --date=iso8601-strict --no-abbrev-commit --abbrev=40 --decorate --all --boundary --pretty=format:'%Cgreen%ad %Cred%h%Creset -%C(yellow)%d%Creset %s %Cblue[%cn <%ce>]%Creset %Cblue%G?%Creset'";
+
+        save = "stash push -u";
+        pop = "stash pop";
+      };
+
+      extraConfig = {
+        branch.autosetuprebase = "always";
+        color.ui = true;
+        core.eol = "lf";
+        core.editor = "nano";
+        diff.algorithm = "histogram";
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.default = "current";
+        rebase.autoStash = true;
+
+        url."git@github.com:".insteadOf = "https://github.com/";
+      };
+    };
+
+    programs.bash = {
+      enable = true;
+
+      bashrcExtra = "
+export XDG_DATA_HOME=\"$HOME/.local/share\"
+source /home/levi/dev/personal/scripts/git-prompt.sh
+
+function changes_in_branch() {
+  if [ -d .git ] || [ -d ../.git ] || [ -d ../../.git ] || [ -d ../../../.git ] || [ -d ../../../../.git ]; then
+  if expr length + \"$(git status -s)\" 2>&1 > /dev/null; then
+    echo -ne \"\\033[0;33m$(__git_ps1)\\033[0m\";
+  else
+    echo -ne \"\\033[0;32m$(__git_ps1)\\033[0m\"; fi;
+  fi
+}
+
+PS1=\"\\[\\033[0;32m\\]\\[\\033[0m\\033[0;32m\\]\\u\\[\\033[0;36m\\] @ \\[\\033[0;36m\\]\\h \\w\\[\\033[0;32m\\]$(__git_ps1)\\n\\[\\033[0;32m\\]└─\\[\\033[0m\\033[0;32m\\] \\$\\[\\033[0m\\033[0;32m\\] ▶\\[\\033[0m\\]  \"";
+
+      shellAliases = {
+        dotfiles-sync = "cat /home/levi/dev/personal/dotfiles/nixos-config/configuration.nix | sudo tee /etc/nixos/configuration.nix";
+
+        gst = "git status";
+        ga = "git add";
+        gcmsg = "git commit -m";
+        gp = "git push";
+        gl = "git pull";
+        gunst = "git restore --staged";
+        gcp = "git cherry-pick";
+        gd = "git diff";
+        gds = "git diff --staged";
+      };
+    };
+  };
+
+  # home-manager.users.levi = {
+  #   dconf.settings = {
+  #     "org/gnome/desktop/peripherals/touchpad" = {
+  #       tap-to-click = true;
+  #       two-finger-scrolling-enabled = true;
+  #     };
+  #   };
+
+  #   "org/gnome/settings-daemon/plugins/media-keys" = {
+  #     custom-keybindings = [
+  #       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+  #       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+  #     ];
+  #   };
+  #   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+  #     name = "Launch Terminal";
+  #     command = "kgx";
+  #     binding = "<Super>T";
+  #   };
+
+  #   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+  #     name = "Launch Flameshot";
+  #     command = "flameshot";
+  #     binding = "<Super><Shift>S";
+  #   };
+  # };
 }
