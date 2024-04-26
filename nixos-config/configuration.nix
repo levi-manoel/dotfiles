9b64055f8cb7be5e72d27aeda0e6fb44e8c4a793
levi-manoel <levimanoel.deob@gmail.com>
2024-04-26 15:18:44 -0300

    chore: start modular config

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -5,6 +5,7 @@
 }: {
   imports = [
     ./hardware.nix
+    ./modules/fonts.nix
   ];
 
   # Bootloader.
@@ -137,60 +138,6 @@
   };
 
   system.stateVersion = "23.11";
-
-  fonts = {
-    fontDir.enable = true;
-
-    enableDefaultPackages = true;
-
-    packages = with pkgs; let
-      mkZedFont = name: hash:
-        stdenv.mkDerivation rec {
-          inherit name;
-          version = "1.2.0";
-
-          src = fetchzip {
-            inherit hash;
-
-            url = "https://github.com/zed-industries/zed-fonts/releases/download/${version}/${name}-${version}.zip";
-            stripRoot = false;
-          };
-
-          installPhase = ''
-            runHook preInstall
-
-            install -Dm644 *.ttf -t $out/share/fonts/truetype
-
-            runHook postInstall
-          '';
-        };
-
-      zed-mono = mkZedFont "zed-mono" "sha256-k9N9kWK2JvdDlGWgIKbRTcRLMyDfYdf3d3QTlA1iIEQ=";
-      zed-sans = mkZedFont "zed-sans" "sha256-BF18dD0UE8Q4oDEcCf/mBkbmP6vCcB2vAodW6t+tocs=";
-    in [
-      victor-mono
-      corefonts
-      font-awesome
-      nerdfonts
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      source-han-sans
-      source-han-sans-japanese
-      source-han-serif-japanese
-      zed-mono
-      zed-sans
-    ];
-
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        monospace = ["Victor Mono" "Zed Mono" "Noto Sans Mono"];
-        serif = ["Noto Serif" "Source Han Serif"];
-        sansSerif = ["Zed Sans" "Noto Sans" "Source Han Sans"];
-      };
-    };
-  };
 
   # NVIDIA
   hardware.opengl = {
