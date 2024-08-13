b8c27f52dd3ea93b968f436e59e96fe2899fefcd
levi-manoel <levimanoel.deob@gmail.com>
2024-08-13 14:57:20 -0300

    stilix: change cursor

--- nixos-config/modules/services/stylix.nix
+++ nixos-config/modules/services/stylix.nix
@@ -21,7 +21,11 @@ in {
       image = mkForce cfg.wallpaper;
       polarity = "dark";
 
-      cursor.size = 24;
+      cursor = {
+        name = "phinger-cursors-dark";
+        package = pkgs.phinger-cursors;
+        size = 24;
+      };
 
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
