2fb6274f42dfa5d19ef38483ba6e20cf63a1b50d
levi-manoel <levimanoel.deob@gmail.com>
2025-06-05 08:43:41 -0300

    chore: chorei again

--- nixos/modules/system/networking.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  networking.networkmanager.enable = true;
-  networking.nameservers = ["8.8.8.8" "8.8.4.4"];
-
-  services.mullvad-vpn.enable = false;
-
-  systemd.packages = [pkgs.cloudflare-warp];
-  systemd.targets.multi-user.wants = ["warp-svc.service"];
-}
