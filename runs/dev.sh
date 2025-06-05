a52f7024f0608c3f767c190abf36596b895cbe8a
levi-manoel <levimanoel.deob@gmail.com>
2025-06-05 14:21:49 -0300

    feat: dev-env script and config files

--- runs/dev.sh
+++ runs/dev.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt -y install git redis-server nodejs npm tmux
+sudo apt -y install git redis-server nodejs npm tmux tldr
 
 git config --global user.email "levimanoel.deob@gmail.com"
 git config --global user.name "levi-manoel"
 
 sudo systemctl start redis-server
 sudo systemctl enable redis-server
+
+tldr --update
 
 sh <(curl -L https://nixos.org/nix/install) --daemon
 nix-env --install --attr devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
