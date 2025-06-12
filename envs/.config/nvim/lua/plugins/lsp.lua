3f239c649d2cadf83575a01e2e9224e7bdd23531
levi-manoel <levimanoel.deob@gmail.com>
2025-06-11 23:03:25 -0300

    commitando

--- envs/.config/nvim/lua/plugins/lsp.lua
+++ envs/.config/nvim/lua/plugins/lsp.lua
@@ -73,16 +73,26 @@ return {
                         capabilities = capabilities,
                         settings = {
                             Lua = {
-                                diagnostics = { enable = true, globals = {"vim"} },
+                                runtime = {
+                                    version = 'LuaJIT',
+                                    path = vim.split(package.path, ';'),
+                                },
+                                diagnostics = {
+                                    enable = true,
+                                    globals = { "vim" },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false, -- Disable annoying prompts
+                                },
                                 format = {
                                     enable = true,
-                                    -- Put format options here
-                                    -- NOTE: the value should be STRING!!
                                     defaultConfig = {
                                         indent_style = "space",
                                         indent_size = "2",
                                     }
                                 },
+                                telemetry = { enable = false },
                             }
                         }
                     }
