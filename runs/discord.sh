f6cd3e6bdfa9964ef3d425242d8ffc66a6b4a523
levi-manoel <levimanoel.deob@gmail.com>
2025-06-18 13:24:22 -0300

    update discord run

--- runs/discord.sh
+++ runs/discord.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-sudo -E gpg --no-default-keyring --keyring=/usr/share/keyrings/javinator9889-ppa-keyring.gpg --keyserver keyserver.ubuntu.com --recv-keys 08633B4AAAEB49FC
-sudo tee /etc/apt/sources.list.d/javinator9889-ppa.list <<< "deb [arch=amd64 signed-by=/usr/share/keyrings/javinator9889-ppa-keyring.gpg] https://ppa.javinator9889.com all main"
-
-sudo apt -y update
-sudo apt -y install discord
+curl -L -o /tmp/discord.deb "https://discord.com/api/download?platform=linux&format=deb"
+sudo apt install /tmp/discord.deb
 
