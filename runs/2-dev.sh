fe681e0ccdfdd1581e970d52ec6afaaf52790317
levi-manoel <levimanoel.deob@gmail.com>
2025-06-18 13:18:01 -0300

    nemseisedacertoveremosumdia

--- runs/2-dev.sh
+++ runs/2-dev.sh
@@ -5,8 +5,7 @@ sudo apt -y install git tmux tldr xclip fzf ripgrep
 tldr --update
 
 sh <(curl -L https://nixos.org/nix/install) --daemon
-source $HOME/.zshrc
-nix-env --install --attr devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
+/nix/var/nix/profiles/default/bin/nix-env --install --attr devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
 
 sudo echo "trusted-users = root $USER" >> /etc/nix/nix.conf
 sudo echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
