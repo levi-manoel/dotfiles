32c71b00678210ca8ecb4bdcbff937bd77d5aa65
levi-manoel <levimanoel.deob@gmail.com>
2025-06-23 13:22:49 -0300

    ..."

--- runs/work.sh
+++ runs/work.sh
@@ -5,6 +5,9 @@ sudo apt -y install redis-server direnv
 sudo systemctl start redis-server
 sudo systemctl enable redis-server
 
+curl -L -o /tmp/slack.deb "https://slack.com/intl/pt-br/downloads/instructions/linux?ddl=1&build=deb"
+sudo apt install /tmp/slack.deb
+
 sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "trusted-users = root $USER" | sudo tee -a /etc/nix/nix.conf
 echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
