885881f76c77939e77c4aec975935632ae9ebab6
levi-manoel <levimanoel.deob@gmail.com>
2025-06-23 09:08:20 -0300

    ..

--- runs/2-dev.sh
+++ runs/2-dev.sh
@@ -4,12 +4,5 @@ sudo apt -y install git tmux tldr xclip fzf ripgrep #lxappearance
 
 tldr --update
 
-sh <(curl -L https://nixos.org/nix/install) --daemon
-/nix/var/nix/profiles/default/bin/nix-env --install --attr devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
-
-sudo echo "trusted-users = root $USER" >> /etc/nix/nix.conf
-sudo echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
-sudo systemctl restart nix-daemon
-
 sudo usermod -aG video $USER
 
--- runs/work.sh
+++ runs/work.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env bash
 
-sudo apt -y install redis-server
+sudo apt -y install redis-server direnv
 
 sudo systemctl start redis-server
 sudo systemctl enable redis-server
+
+sh <(curl -L https://nixos.org/nix/install) --daemon
+echo "trusted-users = root $USER" | sudo tee -a /etc/nix/nix.conf
+echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
+sudo systemctl restart nix-daemon
+
+/nix/var/nix/profiles/default/bin/nix-env --install --attr devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
 
