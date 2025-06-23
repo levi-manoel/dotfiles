b734b1a6509840201c8814d2e2b89283eea8a4c2
levi-manoel <levimanoel.deob@gmail.com>
2025-06-23 14:51:51 -0300

    mintira

--- envs/.zshrc
+++ envs/.zshrc
@@ -30,3 +30,9 @@ export NVM_DIR="$HOME/.nvm"
 
 export PATH=$HOME/dev/personal/dotfiles/bin:$PATH
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/levi/dev/irancho/google-cloud-sdk/path.zsh.inc' ]; then . '/home/levi/dev/irancho/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/levi/dev/irancho/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/levi/dev/irancho/google-cloud-sdk/completion.zsh.inc'; fi
+
--- runs/work.sh
+++ runs/work.sh
@@ -15,3 +15,13 @@ sudo systemctl restart nix-daemon
 
 /nix/var/nix/profiles/default/bin/nix-env --install --attr devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
 
+pushd $HOME/dev/irancho
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-linux-x86_64.tar.gz
+rm google-cloud-cli-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+popd
+
+curl "https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.17.1/cloud-sql-proxy.linux.amd64" -o $HOME/dev/irancho/cloud-sql-proxy
+chmod +x $HOME/dev/irancho/cloud-sql-proxy
+
