445426aa53301929547d0e66148da6624dde15bc
levi-manoel <levimanoel.deob@gmail.com>
2025-08-01 08:55:14 -0300

    alça

--- envs/.wezterm.lua
+++ envs/.wezterm.lua
@@ -4,6 +4,7 @@ return {
   -- Font
   font = wezterm.font("Victor Mono NF"),
   font_size = 16.0,
+  harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' },
 
   -- Colors
   color_scheme = "aiiiii",
--- runs/fonts.sh
+++ runs/fonts.sh
@@ -4,9 +4,10 @@ wget --output-document /tmp/victor-nomo.zip https://github.com/ryanoasis/nerd-fo
 wget --output-document /tmp/mona.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Monaspace.zip
 
 mkdir /tmp/unziped-sources
+
 unzip /tmp/victor-nomo.zip -d /tmp/unziped-sources
 unzip /tmp/mona.zip -d /tmp/unziped-sources
-sudo mv /tmp/unziped-sources/*.ttf /usr/local/share/fonts/ 
 
+sudo mv /tmp/unziped-sources/*.ttf /usr/local/share/fonts/ 
 fc-cache -fv
 
