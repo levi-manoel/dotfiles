0f1a72089b855ae6f8e66f02279c4cb9c4c352c1
levi-manoel <levimanoel.deob@gmail.com>
2025-09-23 18:16:22 -0300

    irraaaaa

--- runs/2-dev.sh
+++ runs/2-dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 yay -Syu --noconfirm \
-  git tmux tldr xclip fzf ripgrep rsync wget rsync meson cpio cmake
+  git tmux tldr xclip fzf ripgrep rsync wget rsync meson cpio cmake beekeeper-studio
 
 hyprpm update
 hyprpm add https://github.com/outfoxxed/hy3
