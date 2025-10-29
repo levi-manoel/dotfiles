731a93cf68fedd5de3e4b67baa9b31a45e3f8479
levi-manoel <levimanoel.deob@gmail.com>
2025-10-29 14:33:13 -0300

    ai papai

--- envs/.config/nvim/lua/plugins/copilot.lua
+++ envs/.config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,11 @@
 return {
     "github/copilot.vim",
+
+    config = function()
+        vim.g.copilot_node_command = "/home/levi/.nvm/versions/node/v22.20.0/bin/node"
+        vim.g.copilot_filetypes = {
+            ["*"] = true,
+        }
+    end,
 }
 
