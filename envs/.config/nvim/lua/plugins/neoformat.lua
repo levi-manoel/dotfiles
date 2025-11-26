5885d6aca7148ebda23981116cc72fafa97d3849
levi-manoel <levimanoel.deob@gmail.com>
2025-11-26 16:12:43 -0300

    aii

--- envs/.config/nvim/lua/plugins/neoformat.lua
+++ envs/.config/nvim/lua/plugins/neoformat.lua
@@ -8,8 +8,34 @@ return {
             pattern = { "*.ts", "*.js", "*.vue" },
             callback = function()
                 vim.cmd("Neoformat")
+
+
             end,
         })
+
+        local function run_prettier_npx()
+            local filepath = vim.fn.expand('%')
+
+            if filepath == '' then
+                print("Buffer has no file path.")
+                return
+            end
+
+            vim.cmd('write')
+
+            local output = vim.fn.system('npx prettier --write ' .. vim.fn.shellescape(filepath))
+
+            if vim.v.shell_error ~= 0 then
+                print("Prettier Error: " .. output)
+            else
+                vim.cmd('edit')
+                print("Prettier formatted: " .. filepath)
+            end
+        end
+
+        vim.api.nvim_create_user_command('PrettierNpx', run_prettier_npx, {})
+
+        vim.keymap.set('n', '<leader>p', run_prettier_npx, { desc = 'Run npx prettier --write' })
     end,
 }
 
