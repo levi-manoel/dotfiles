6548504a3ba33da018dd8aa2799f15cd6ab6360f
levi-manoel <levimanoel.deob@gmail.com>
2026-01-22 08:13:03 -0300

    chore: comment format on write

--- envs/.config/nvim/lua/plugins/neoformat.lua
+++ envs/.config/nvim/lua/plugins/neoformat.lua
@@ -4,12 +4,12 @@ return {
         vim.g.neoformat_try_node_exe = 1
     end,
     config = function()
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = { "*.ts", "*.js", "*.vue" },
-            callback = function()
-                vim.cmd("Neoformat")
-            end,
-        })
+        -- vim.api.nvim_create_autocmd("BufWritePre", {
+        --     pattern = { "*.ts", "*.js", "*.vue" },
+        --     callback = function()
+        --         vim.cmd("Neoformat")
+        --     end,
+        -- })
 
         local function run_prettier_npx()
             local filepath = vim.fn.expand('%')
