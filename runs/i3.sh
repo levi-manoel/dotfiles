af0dbb44445fb9476d6ba04ed8c021739480f4a5
levi-manoel <levimanoel.deob@gmail.com>
2026-04-07 15:33:08 -0300

    oqueee

--- runs/i3.sh
+++ runs/i3.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 sudo dnf copr enable -y alternateved/i3status-rust
 
-sudo dnf install -y i3 i3blocks rofi dex i3lock i3status-rust
+# sudo dnf install -y i3 i3blocks rofi dex i3lock i3status-rust
+sudo dnf install -y i3status-rust
--- runs/spotify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-spotify.repo
-
-sudo dnf install -y spotify-client spotify-ffmpeg
